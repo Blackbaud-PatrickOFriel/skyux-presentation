e6ed87f2f06bdf89ec7d7222e8b89e0993a1fc92
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-07-27 12:05:45 -0400

    Add component example

---
+++ austin-angularjs-training/example-03/index.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    
+    function SimpleTestController() {
+        var vm = this;
+        
+        vm.isBlue = true;
+    }
+    
+    angular.module('presentation', [])
+        .component('bbSimple', {
+            template: '<div class="bb-simple-background" ng-class="\'bb-simple-background-blue\': $ctrl.bbSimpleIsBlue"><ng-transclude></ng-transclude></div>',
+            controller: Controller,
+            bindings: {
+                bbSimpleIsBlue: '<?'
+            },
+            transclude: true
+        })
+        .controller('SimpleTestController', SimpleTestController);
+}());
