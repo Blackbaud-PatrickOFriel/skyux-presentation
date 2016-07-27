ff20b197bcd982eb70629ce11ede94143b571e3a
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-07-27 12:11:56 -0400

    update dependencies

--- austin-angularjs-training/example-03/index.js
+++ austin-angularjs-training/example-03/index.js
@@ -7,7 +7,7 @@
         vm.isBlue = true;
     }
     
-    angular.module('skytutorial', [])
+    angular.module('skytutorial', ['sky'])
         .component('bbSimple', {
             template: '<div class="bb-simple-background" ng-class="\'bb-simple-background-blue\': $ctrl.bbSimpleIsBlue"><ng-transclude></ng-transclude></div>',
             controller: angular.noop,
