1cc750f28f8eba9c3a36d0f894dbb630da80db76
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2016-08-09 13:26:20 -0400

    Add charleston presentation

---
+++ chs-angularjs-training/example-03/index.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    
+    function SimpleTestController() {
+        var vm = this;
+        
+        vm.isBlue = true;
+    }
+    
+    angular.module('skytutorial', ['sky'])
+        .component('bbSimple', {
+            template: '<div class="bb-simple-background" ng-class="{\'bb-simple-background-blue\': $ctrl.bbSimpleIsBlue}"><ng-transclude></ng-transclude></div>',
+            controller: angular.noop,
+            bindings: {
+                bbSimpleIsBlue: '<?'
+            },
+            transclude: true
+        })
+        .controller('SimpleTestController', SimpleTestController);
+}());
